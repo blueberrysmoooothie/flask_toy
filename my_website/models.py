5021b85b3185c57e8f14e4ddce1e64614264dcac
loading <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-08-18 10:59:21 +0900

    secret

--- my_website/models.py
+++ my_website/models.py
@@ -1,4 +1,6 @@
 from flask import session
+from passlib.hash import pbkdf2_sha256
+from secret import SALT
 
 
 class User:
@@ -10,7 +12,7 @@ class User:
         if id in User.user_list_:
             user_info = User.user_data_[id]
 
-            if password == user_info["user_password_"]:
+            if cls.check_password(password, user_info["user_password_"]):
                 session["user_name_"] = user_info["user_name_"]
                 session["user_id_"] = id
                 return True
@@ -21,12 +23,24 @@ class User:
         session.clear()
 
     @classmethod
-    def add_user_info(cls, nickname, userid, password1):
+    def add_user_info(cls, nickname, userid, password):
         User.user_list_.append(userid)
-        User.user_data_[userid] = {"user_name_": nickname, "user_password_": password1}
+        User.user_data_[userid] = {
+            "user_name_": nickname,
+            "user_password_": cls.hash_password(password),
+        }
 
     @classmethod
     def dup_check(cls, userid):
         if userid in User.user_list_:
             return True
         return False
+
+    @classmethod
+    def hash_password(cls, password):
+        return pbkdf2_sha256.hash(password + SALT)
+
+    @classmethod
+    def check_password(cls, password, hashed_password):
+        check = pbkdf2_sha256.verify(password + SALT, hashed_password)
+        return check
