a2e58ac3cde4076ead59c84c5c8bbcba9aa14290
loading <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-08-23 20:04:09 +0900

    db

---
+++ my_website/module/dbModule.py
@@ -0,0 +1,33 @@
+import pymysql
+from settings import DB_HOST, DB_ADMIN_ID, DB_ADMIN_PW, DB_NAME
+
+
+class Database:
+    def __init__(self):
+        self.db = pymysql(
+            host=DB_HOST,
+            user=DB_ADMIN_ID,
+            passwd=DB_ADMIN_PW,
+            db=DB_NAME,
+            charset="utf8",
+        )
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+
+    def execute(self, query, args={}):
+        if "values" in args:
+            self.cursor.executemany(query, args["values"])
+        else:
+            self.cursor.execute(query, args)
+
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchone()
+        return row
+
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+
+    def commit(self):
+        self.db.commit()
---
+++ settings.py
@@ -0,0 +1,7 @@
+from secret import DB_ADMIN_ID, DB_ADMIN_PW
+
+DB_HOST = "mysqldb.cubr5jfdwaj7.us-east-2.rds.amazonaws.com:3306"
+DB_ADMIN_ID = DB_ADMIN_ID
+DB_ADMIN_PW = DB_ADMIN_PW
+DB_NAME = "web_game_db"
+ADMIN_ID = "test"
